4c8c7f8d821ec2048f0f38091e4d85dc07cf8986
Aedo F <f.aedo@duocuc.cl>
2025-04-16 02:59:41 -0400

    Adicion de botones y logica para carrito

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 
 # Create your models here.
 class Usuario(models.Model):
@@ -29,3 +31,35 @@ class Juego(models.Model):
 
     def __str__(self):
         return self.nombre_juego
+    
+def agregar_al_carrito(request, codjuego):
+    juego = get_object_or_404(Juego, codjuego=codjuego)
+
+    carrito = request.session.get('carrito', {})
+
+    if str(codjuego) in carrito:
+        carrito[str(codjuego)]['cantidad'] += 1
+    else:
+        carrito[str(codjuego)] = {
+            'nombre': juego.nombre_juego,
+            'precio': float(juego.precio),
+            'caratula': juego.caratula.url,
+            'cantidad': 1
+        }
+
+    request.session['carrito'] = carrito
+    request.session.modified = True
+    return redirect('catalogo')
+
+def ver_carrito(request):
+    carrito = request.session.get('carrito', {})
+    total = sum(item['precio'] * item['cantidad'] for item in carrito.values())
+    envio = 5000
+    total_final = total + envio
+
+    return render(request, 'carrito.html', {
+        'carrito': carrito,
+        'subtotal': total,
+        'envio': envio,
+        'total': total_final
+    })
