5ff192a1c1e0b864e8d530c96ee66da91651ef3a
Aedo F <f.aedo@duocuc.cl>
2025-04-18 19:31:27 -0400

    Creacion automatica de categorias

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+    def ready(self):
+        from core.models import CATEGORY
+        categorias = ['Adventure', 'Shooter', 'Racing', 'Terror', 'Strategy']
+        for nombre in categorias:
+            CATEGORY.objects.get_or_create(nombre=nombre)
