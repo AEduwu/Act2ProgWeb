bdeb970713794696e6bc9d145bc6139c056944d9
Aedo F <f.aedo@duocuc.cl>
2025-05-09 22:03:50 -0400

    Complejizacion base de datos

--- core/models.py
+++ core/models.py
@@ -39,3 +39,28 @@ class GAME(models.Model):
     def __str__(self):
         return self.game_name   
     
+class MEDIO_PAGO(models.Model):
+    cod_medio = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class BOLETA(models.Model):
+    cod_boleta = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    total = models.IntegerField()
+    user = models.ForeignKey('USER', on_delete=models.CASCADE)
+    medio_pago = models.ForeignKey('MEDIO_PAGO', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'Boleta #{self.cod_boleta} - {self.user.email}'
+
+class DETALLE_BOLETA(models.Model):
+    boleta = models.ForeignKey('BOLETA', on_delete=models.CASCADE, related_name='detalles')
+    game = models.ForeignKey('GAME', on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
+    subtotal = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.cantidad} x {self.game.game_name} (Boleta #{self.boleta.cod_boleta})'
